744d9287cdb177702ff95e357c9d641e35e25514
Alejandro <contact@alejandrosanchez.dev>
2025-05-18 11:40:55 -0500

    shadcn init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button className="text-teal-600">Test</Button>
+    </div>
+  )
+}
